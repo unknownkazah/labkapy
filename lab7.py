6db2f4512c7fbfa0e90ac8d96b3cbde7ac1a5da8
unknownkazah <119103885+unknownkazah@users.noreply.github.com>
2023-11-25 10:50:51 +0600

    update lab8

--- lab7.py
+++ lab7.py
@@ -1,20 +1,20 @@
 import random
 
-
+file_path = '/Users/rakhmet_zhanserik/Desktop/labkapy/text.txt'
 def read_and_display(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             for line in file:
                 print(line.strip())
     except FileNotFoundError:
         print("File not found.")
 
 # Usage
-read_and_display('text1.txt')
+read_and_display('text.txt')
 
 def read_random_line(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             lines = file.readlines()
             random_line = random.choice(lines)
             print(random_line.strip())
@@ -22,11 +22,11 @@ def read_random_line(file_name):
         print("File not found.")
 
 # Usage
-read_random_line('text1.txt')
+read_random_line('text.txt')
 
 def count_uppercase_chars(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             content = file.read()
             uppercase_count = sum(1 for char in content if char.isupper())
             print("Uppercase characters count:", uppercase_count)
@@ -34,11 +34,11 @@ def count_uppercase_chars(file_name):
         print("File not found.")
 
 # Usage
-count_uppercase_chars('text1.txt')
+count_uppercase_chars('text.txt')
 
 def count_lines_not_starting_with_D(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             lines = file.readlines()
             count = sum(1 for line in lines if not line.startswith('D'))
             print("Number of lines not starting with 'D':", count)
@@ -46,12 +46,12 @@ def count_lines_not_starting_with_D(file_name):
         print("File not found.")
 
 # Usage
-count_lines_not_starting_with_D('text1.txt')
+count_lines_not_starting_with_D('text.txt')
 
 
 def count_words_ending_with_F(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             content = file.read()
             words = content.split()
             count = sum(1 for word in words if word.lower().endswith('f'))
@@ -60,11 +60,11 @@ def count_words_ending_with_F(file_name):
         print("File not found.")
 
 # Usage
-count_words_ending_with_F('text1.txt')
+count_words_ending_with_F('text.txt')
 
 def count_all_and_none_words(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             content = file.read()
             words = content.split()
             all_count = words.count("all")
@@ -75,11 +75,11 @@ def count_all_and_none_words(file_name):
         print("File not found.")
 
 # Usage
-count_all_and_none_words('text1.txt')
+count_all_and_none_words('text.txt')
 
 def count_word_frequency(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             content = file.read()
             words = content.split()
             word_freq = {}
@@ -90,12 +90,12 @@ def count_word_frequency(file_name):
         print("File not found.")
 
 # Usage
-count_word_frequency('text1.txt')
+count_word_frequency('text.txt')
 
 
 def find_longest_word(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             content = file.read()
             words = content.split()
             longest_word = max(words, key=len)
@@ -104,11 +104,11 @@ def find_longest_word(file_name):
         print("File not found.")
 
 # Usage
-find_longest_word('text1.txt')
+find_longest_word('text.txt')
 
 def replace_B_with_J(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             content = file.read()
             corrected_content = content.replace('B', 'J')
             print("Corrected content:")
@@ -117,11 +117,11 @@ def replace_B_with_J(file_name):
         print("File not found.")
 
 # Usage
-replace_B_with_J('text1.txt')
+replace_B_with_J('text.txt')
 
 def count_A_and_B_characters(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_path, 'r') as file:
             content = file.read()
             count_a = content.lower().count('a')
             count_b = content.lower().count('b')
@@ -130,7 +130,7 @@ def count_A_and_B_characters(file_name):
         print("File not found.")
 
 # Usage
-count_A_and_B_characters('text1.txt')
+count_A_and_B_characters('text.txt')
 
 
 
